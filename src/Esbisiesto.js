4c9e6da5f699d5d3242e2bb8dd8ce57a9dd13ecd
Dylan Calle <dylan.calle@ucb.edu.bo>
2024-08-26 21:09:56 -0400

    divisibles por 4 pero no por 100 si son

--- src/Esbisiesto.js
+++ src/Esbisiesto.js
@@ -1,16 +1,16 @@
 function Esbisiesto(numero) {
     let resul = false
-    if(numero % 400 == 0)
-    {
-        resul= true
-    }else{
-        resul= false
-    }
     if(numero % 100 == 0 && numero % 400 != 0)
     {
         resul = false
     }else{
         resul = true
+    }
+    if(numero % 4 == 0 && (numero % 100 != 0 || numero % 400 == 0))
+    {
+        resul = true
+    }else{
+        resul = false
     }
     
     return resul
--- src/Esbisiesto.spec.js
+++ src/Esbisiesto.spec.js
@@ -2,9 +2,12 @@ import Esbisiesto from "./Esbisiesto.js";
 
 describe("Es bisisesto", () => {
   it("deberia devolver true si es divisible entre 400", () => {
-    expect(Esbisiesto(400)).toEqual(true);
+    expect(Esbisiesto(2400)).toEqual(true);
   });
   it("deberia devolver false si el año es divisible por 100 pero no por 400", () => {
     expect(Esbisiesto(1700)).toEqual(false);
+  });
+  it("deberia devolver true si el año es divisible por 4 pero no por 100", () => {
+    expect(Esbisiesto(2008)).toEqual(true);
   });
 });
